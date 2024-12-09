4cf84217a76fa443889c56a1f2a23d1c18e641a8
a1exx2a <a1exx2a@yandex.ru>
2024-12-09 17:20:17 +0300

    init commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task7;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] x = {1,2,3,4,5,6,7,8,9,10};
+        
+        for (int i = 0; i < x.Length; i++){
+            if (i%2 < 1){
+                Console.WriteLine(x[i]);
+            }
+        }
+    }
+}
